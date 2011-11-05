ace3c1524a57c348f83ca724011cf2107baa72fa
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2011-11-05 20:33:42 +0100

    lftp 4.3.3

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Lftp < Formula
-  url 'http://ftp.yars.free.net/pub/source/lftp/lftp-4.3.1.tar.bz2'
+  url 'http://ftp.yar.ru/pub/source/lftp/lftp-4.3.3.tar.gz'
   homepage 'http://lftp.yar.ru/'
-  md5 'ea45acfb47b5590d4675c50dc0c6e13c'
+  md5 '14bd2e4a583d07593d76da3690aef02a'
 
   depends_on 'pkg-config' => :build
   depends_on 'readline'
